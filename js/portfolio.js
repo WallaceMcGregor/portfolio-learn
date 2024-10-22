0d7f3d0ae2a5c1f8596bdf290abb4826b6452081
WallaceMcGregor <wallace0mcgregor@gmail.com>
2024-10-22 07:54:04 +0200

    Añadir el enlace a Rick para las RRSS

---
+++ js/portfolio.js
@@ -0,0 +1,7 @@
+let enviar = document.getElementsByClassName(["fa-square-github", "fa-linkedin", "fa-square-twitter", "fa-square-instagram"]);
+
+function rick () {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+};
+
+enviar.onclick = rick();
